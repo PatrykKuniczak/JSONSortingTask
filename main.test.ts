9858d42ad13b9536a7fca28c9fb9cb55b171b723
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-12-18 01:03:42 +0100

    create new test and rename

--- main.test.ts
+++ main.test.ts
@@ -1,7 +1,7 @@
 import {removeDuplicates} from "./main";
 
 
-const actualJSON = JSON.stringify([{
+const beforeJSON = JSON.stringify([{
     "type": "product",
     "id": "3663602537083",
     "attributes": {
@@ -135,10 +135,188 @@ const secondWrongJSON = JSON.stringify([{
 }
 ]);
 
+const secondBeforeJSON = JSON.stringify([{
+    "type": "product",
+    "id": "3663602537083",
+    "attributes": {
+        "ean": "3663602537083",
+        "name": "GoodHome Halland White Oak test asuwu Real wood top layer flooring, 1.37m² Pack",
+        "pricing": {
+            "currencyCode": "GBP",
+            "currentPrice": {
+                "amountIncTax": 54.8,
+                "amountExTax": 45.67
+            },
+            "dupa": {
+                "wolowa": false,
+                "cipowa": true,
+                "inna": {
+                    "tak": false,
+                    "nie": true
+                }
+            }
+        }
+    }
+}, {
+    "type": "product",
+    "id": "3663602537085",
+    "attributes": {
+        "ean": "3663602537085",
+        "name": "GoodHome Halland White Oak test asuwu Real wood top layer flooring, 1.37m² Pack",
+        "pricing": {
+            "currentPrice": {
+                "amountExTax": 49
+            },
+            "dupa": {
+                "cipowa": false,
+                "inna": {
+                    "tak": true,
+                    "nie": false
+                }
+            },
+            "taxes": [
+                {
+                    "type": "VAT",
+                    "amount": 9.13,
+                    "rate": 25
+                }],
+        }
+    }
+}
+]);
 
-test("should return Array without duplicates (between every each scope)", () => {
-    expect(removeDuplicates(actualJSON)).toEqual(requiredList);
-    expect(removeDuplicates(actualJSON)).not.toEqual(actualJSON);
-    expect(removeDuplicates(actualJSON)).not.toEqual(wrongJSON)
-    expect(removeDuplicates(actualJSON)).not.toEqual(secondWrongJSON)
+const secondRequiredJSON = JSON.stringify([{
+    "type": "product",
+    "id": "3663602537083",
+    "attributes": {
+        "ean": "3663602537083",
+        "name": "GoodHome Halland White Oak test asuwu Real wood top layer flooring, 1.37m² Pack",
+        "pricing": {
+            "currencyCode": "GBP",
+            "currentPrice": {
+                "amountIncTax": 54.8,
+                "amountExTax": 45.67
+            },
+            "dupa": {
+                "wolowa": false,
+                "cipowa": true,
+                "inna": {
+                    "tak": false,
+                    "nie": true
+                }
+            }
+        }
+    }
+}, {
+    "id": "3663602537085",
+    "attributes": {
+        "ean": "3663602537085",
+        "pricing": {
+            "currentPrice": {
+                "amountExTax": 49
+            },
+            "dupa": {
+                "cipowa": false,
+                "inna": {
+                    "tak": true,
+                    "nie": false
+                }
+            },
+            "taxes": [
+                {
+                    "type": "VAT",
+                    "amount": 9.13,
+                    "rate": 25
+                }],
+        }
+    }
+}
+]);
+
+const thirdBeforeJSON = JSON.stringify([{
+    "type": "product",
+    "id": "3663602537083",
+    "attributes": {
+        "ean": "3663602537083",
+        "name": "GoodHome Halland White Oak test asuwu Real wood top layer flooring, 1.37m² Pack",
+        "pricing": {
+            "currencyCode": "GBP",
+            "currentPrice": {
+                "amountIncTax": 54.8,
+                "amountExTax": 45.67
+            },
+            "dupa": {
+                "wolowa": false,
+                "cipowa": true,
+                "inna": {
+                    "tak": false,
+                    "nie": true
+                }
+            }
+        }
+    }
+}, {
+    "type": "product",
+    "id": "3663602537085",
+    "attributes": {
+        "ean": "3663602537085",
+        "name": "GoodHome Halland White Oak test asuwu Real wood top layer flooring, 1.37m² Pack",
+        "pricing": {
+            "currencyCode": "GBP",
+            "currentPrice": {
+                "amountIncTax": 54.8,
+                "amountExTax": 45.67
+            },
+            "dupa": {
+                "wolowa": false,
+                "cipowa": true,
+                "inna": {
+                    "tak": false,
+                    "nie": true
+                }
+            }
+        }
+    }
+}
+]);
+
+const thirdRequiredJSON = JSON.stringify([{
+    "type": "product",
+    "id": "3663602537083",
+    "attributes": {
+        "ean": "3663602537083",
+        "name": "GoodHome Halland White Oak test asuwu Real wood top layer flooring, 1.37m² Pack",
+        "pricing": {
+            "currencyCode": "GBP",
+            "currentPrice": {
+                "amountIncTax": 54.8,
+                "amountExTax": 45.67
+            },
+            "dupa": {
+                "wolowa": false,
+                "cipowa": true,
+                "inna": {
+                    "tak": false,
+                    "nie": true
+                }
+            }
+        }
+    }
+}, {
+    "id": "3663602537085",
+    "attributes": {
+        "ean": "3663602537085"
+    }
+}
+]);
+
+test("should return Array without duplicates (between every each scope/nested lvl)", () => {
+    expect(removeDuplicates(beforeJSON)).toEqual(requiredList);
+    expect(removeDuplicates(beforeJSON)).not.toEqual(beforeJSON);
+    expect(removeDuplicates(beforeJSON)).not.toEqual(wrongJSON)
+    expect(removeDuplicates(beforeJSON)).not.toEqual(secondWrongJSON)
+
+    expect(removeDuplicates(secondBeforeJSON)).toEqual(secondRequiredJSON);
+
+    expect(removeDuplicates(thirdBeforeJSON)).toEqual(thirdRequiredJSON);
 })
